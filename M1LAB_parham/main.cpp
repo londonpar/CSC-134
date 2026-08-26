2784298a10acf7668fbc639cacfb299c102c0f91
londonpar <londonparham8581@gmail.com>
2026-08-26 11:21:46 -0400

    M1LAB1

---
+++ M1LAB_parham/main.cpp
@@ -0,0 +1,33 @@
+// CSC 134
+// M1LAB - apple sales
+// parhaml
+// 8/26/26
+// Calculate apple prices.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   // Introduce yourself
+   string name = "Parham"; // your own name here
+   string product = "apple"; // feel free to change
+
+   cout << "Welcome to the " << name <<" ";
+   cout << product << " farm." << endl;
+
+   // Do product price calculations
+   int product_count = 100;
+   double product_price = .50; // in USD
+
+   cout << "We have " << product_count << " ";
+   cout << product << "(s). They are $" << product_price;
+   cout << " each." << endl;
+
+   double total_price = product_count * product_price;
+   cout << "Total price for all " << product_count;
+   cout << " is: $" << total_price << endl;
+
+       return 0;
+}
