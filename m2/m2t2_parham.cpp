0fbda8dc8900a1a86e726a3cfad48e9d2733eb71
londonpar <londonparham8581@gmail.com>
2026-09-14 14:46:32 +0000

    m2t1 and m2t2 name change

---
+++ m2/m2t2_parham.cpp
@@ -0,0 +1,31 @@
+// CSC 134
+// MT21 - Receipt
+// Parhaml
+// 9/14/26
+// Build a receipt that looks like a receipt.
+
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    // DECLARE VARIABLES
+    string meal_name;       // ex: "Chicken sandwhich"
+    double meal_price;      // $
+    double tax_rate;        // Percent
+    double tax_amount;      // $
+    double total;           // $, meal + tax
+
+    // INPUT
+    // Right now, nothing. They pick exactly one sandwich.
+    // For now, hard code some values
+    meal_name = "Chicken Sandwich"; 
+    meal_price =  5.99;
+    tax_rate = 0.08; // 8%
+
+    // PROCESSING
+
+    // OUTPUT
+
+    return 0; // no errors
+}
